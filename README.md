2b607e2c0f5417d09a9b7d69ddac36828befde2d
Eunice Park <28845173+eunicode@users.noreply.github.com>
2020-01-07 02:27:02 -0800

    ESLint (my defined rules, es6), Prettier work

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # boilerplate-eslint-prettier-basic
+
+At this point,
+
+the only ESLint rules applied are the ones I specified in .eslintrc.json file.
+ESLint works. It displays squiggly lines.
+ESLint works with ES6 syntax.
+
+Prettier works. It automatically formats my defined rules on save.
---
+++ notes.js
@@ -0,0 +1,146 @@
+/* =================================================================  
+                          CURRENT STATE
+================================================================= */
+
+/*
+4 DEPENDENCIES
+eslint
+eslint-config-prettier
+eslint-plugin-prettier
+prettier
+
+ESLINT RULES
+"no-console": 0,
+"func-names": 0,
+"consistent-return": 0,
+"prettier/prettier": "error"
+
+PRETTIER RULES
+"tabWidth": 2,
+"printWidth": 80,
+"singleQuote": true,
+"semi": true
+
+ESLINT CONFIG
+"extends": ["prettier"]
+"plugins": ["prettier"]
+
+MY NOTES
+The only ESLint rules applied are the ones I specified in config file.
+ESLint works. It displays squiggly lines.
+Prettier works. It automatically formats my defined rules on save.
+
+--------------------------------------------------------------------
+https://eslint.org/docs/user-guide/getting-started
+
+npm install eslint --save-dev
+
+https://thomlom.dev/setup-eslint-prettier-react/
+
+npm install prettier --save-dev
+npm install --save-dev eslint-config-prettier eslint-plugin-prettier
+
+eslint-config-prettier disables rules that conflict with Prettier  
+eslint-plugin-prettier adds the rule that format content using Prettier
+
+ */
+
+/* =================================================================  
+                              NOTES
+================================================================= */
+
+/*
+Configuring ESLint
+https://eslint.org/docs/user-guide/configuring
+
+Configuration File Formats
+
+ESLint supports configuration files in several formats:
+- JavaScript - use .eslintrc.js and export an object containing your configuration.
+- JSON - use .eslintrc.json to define the configuration structure. 
+ESLint's JSON files also allow JavaScript-style comments.
+
+If there are multiple configuration files in the same directory, ESLint will only use one. 
+The priority order is:
+1. .eslintrc.js
+2. .eslintrc.cjs
+3. .eslintrc.yaml
+4. .eslintrc.yml
+5. .eslintrc.json
+6. .eslintrc
+7. package.json
+
+--------------------------------------------------------------------
+.ESLINTRC.JSON
+
+ESLint rule
+0 - turns the rule off // "off"
+1 - turn the rule on as a warning (doesn't affect exit code) // "warn"
+2 - turn the rule on as an error (exit code is 1 when triggered) // "error"
+
+--------------------------------------------------------------------
+HOW TO WRITE RULES 
+
+"arrow-parens": ["error", "always"]  // my guess
+
+"eqeqeq": "off"
+"eqeqeq": "warn"
+"eqeqeq": 0
+"eqeqeq": ["error", "allow-null"]
+"eqeqeq": ["warn", "allow-null"]
+
+--------------------------------------------------------------------
+PLUGIN RULES
+
+To configure a rule which is defined within a plugin you have to prefix the rule ID with the plugin name and a /. 
+For example:
+"plugin1/rule1": "error"
+
+--------------------------------------------------------------------
+ESLINT RULES - EFFECTS
+
+warn 
+- shows lavender squiggly line in code. When you hover over squiggly line, you get a pop-up with an explanation message
+Shows up as warning in ESLint terminal
+Shows up as warning in ESLint terminal
+
+error 
+- shows red squiggly line in code.
+
+Shows up as error in ESLint terminal (Expected '===' and instead saw '=='.)
+Shows up as error in Prettier terminal
+
+ESLint rules do NOT get automatically fixed on save.
+Prettier rules DO get automatically fixed on save.
+
+--------------------------------------------------------------------
+https://thomlom.dev/setup-eslint-prettier-react/
+
+ESLINT
+
+plugins
+Basically, plugins are a set of ESLint rules related to a same subject. As an example, eslint-plugin-react contains many rules related to React.
+Caution: you have to install the plugin as a dev dependency if you want your rules to work properly.
+
+extends
+Allows you to extend your configuration file from others configurations. 
+A popular one is react-app provided by Create React App. 
+That's also in extends that you can use popular style guides such as the one of Airbnb, Google or Standard.
+
+--------------------------------------------------------------------
+https://eslint.org/docs/user-guide/configuring
+
+Specifying Parser Options
+
+ESLint allows you to specify the JavaScript language options you want to support. 
+By default, ESLint expects ECMAScript 5 syntax. 
+You can override that setting to enable support for other ECMAScript versions as well as JSX by using parser options.
+We recommend using eslint-plugin-react if you are using React and want React semantics. 
+By the same token, supporting ES6 syntax is not the same as supporting new ES6 globals (e.g., new types such as Set). 
+
+For ES6 syntax, use { "parserOptions": { "ecmaVersion": 6 } }; 
+for new ES6 global variables, use { "env": { "es6": true } }. 
+
+{ "env": { "es6": true } } enables ES6 syntax automatically, 
+but { "parserOptions": { "ecmaVersion": 6 } } does not enable ES6 globals automatically.
+*/
